cf27df5430d1be0457aae9dceb172f0f3da4f18b
Joshua Trevor <u6405233@anu.edu.au>
2019-03-18 01:26:44 +1100

    Implemented reading tag mappings from config file

---
+++ src/TaxonomyExtractXML.py
@@ -0,0 +1,18 @@
+def get_tag_dict():
+    tag_source_file = open("../tags_config.txt", 'r')
+    tag_dict = {}
+    for line in tag_source_file.readlines():
+        if line[0] == "#" or len(line) < 5:
+            continue
+        target_line = line.replace("\n", "")
+        target_line = target_line.replace(" ", "")
+        try:
+            tags, category = target_line.split("->")
+        except:
+            print("The config file may be formatted incorrectly, the following line could not be parsed: \n{}\n "
+                  "please make sure each line is in the format a, b, c ... -> d".format(line))
+            exit(0)
+        for tag in tags.split(","):
+            tag_dict[tag] = category
+    tag_source_file.close()
+    return tag_dict
