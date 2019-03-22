99dff996b89ae18c238494771ae6009889d8fe77
JingLi201802 <48374334+jingli201802@users.noreply.github.com>
2019-03-22 12:42:10 +1100

    Extract new genus and species use the 26new species xml as example

--- src/TaxonomyExtractXML.py
+++ src/TaxonomyExtractXML.py
@@ -1,3 +1,6 @@
+import xml.etree.ElementTree as ET
+import pandas as pd
+
 def get_tag_dict():
     """Reads tags_config.txt and maps each of the tags to their data category label. Returns a dictionary."""
     tag_source_file = open("../tags_config.txt", 'r')
@@ -17,4 +20,43 @@ def get_tag_dict():
             tag_dict[tag] = category
     tag_source_file.close()
     return tag_dict
+
+
+
+
+
+allinfoList=list()
+tempGenSpe=dict()  # {'genus':genus_name,''species':species_name}
+tempGenSpe['genus']=''
+tempGenSpe['species']=''
+
+df_nonEmpty=pd.DataFrame(columns=['genus','species'])
+
+
+def get_species_related():
+    global df_nonEmpty
+    spStructure='front/article-meta/abstract/p/italic/{http://www.plazi.org/taxpub}taxon-name/{http://www.plazi.org/taxpub}taxon-name-part'
+
+    root=ET.parse('../26newSpecies.xml')
+
+
+    for item in root.iterfind(spStructure):
+        if item.attrib['taxon-name-part-type']=='genus':
+            tempGenSpe['genus']=item.attrib['reg']
+        if item.attrib['taxon-name-part-type']=='species':
+            tempGenSpe['species']=item.attrib['reg']
+
+        if tempGenSpe['species']!='':
+            insertRow=pd.Series([tempGenSpe['genus'],tempGenSpe['species']],index=['genus','species'])
+            df_nonEmpty=df_nonEmpty.append(insertRow,ignore_index=True)
+            tempGenSpe['genus']=''
+            tempGenSpe['species']=''
+    print(df_nonEmpty)
+
+
+get_species_related()
+
+
+
+
 
