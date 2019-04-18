6d671540b0985dce7d6ea23b7bb022d3b63824f8
Jing Li <u6531952@anu.edu.au>
2019-04-18 10:02:16 +1000

    Extract genus and species in body

--- src/TaxonomyExtractXML.py
+++ src/TaxonomyExtractXML.py
@@ -31,7 +31,7 @@ tempGenSpe['species']=''
 df_nonEmpty=pd.DataFrame(columns=['genus','species'])
 
 
-articleName='../aTerrestrialFrog.xml'
+articleName='../26newSpecies.xml'
 tree=ET.parse(articleName)
 root=tree.getroot()
 
@@ -151,44 +151,5 @@ snpInBodySys()
 
 
 
-# import xml.etree.ElementTree as parse
-# root = parse.parse('../xmlExample.xml')    # Parse XML file
-# doc = root.getroot()
 
-
-# print(doc.tag)
-# print(doc.attrib)
-
-
-# rootT = root.find('channel/title')
-# rootL = root.find('channel/link')
-# rootD = root.find('channel/description')
-# print(rootT.tag, rootT.attrib)
-# print(rootL.tag, rootL.attrib)
-# print(rootD.tag, rootD.attrib)
-# print(rootT.text)
-# print(rootL.text)
-# print(rootD.text)
-
-
-
-# for c in root.iterfind('channel'):
-#     print(c.findtext('title'))
-#     print(c.findtext('link'))
-#     print(c.findtext('description'))
-
-#
-# for item in root.iterfind('channel/item/values'):
-#     print(item.findtext('title'))
-#     print(item.findtext('link'))
-#     print(item.findtext('description'))
-#
-#
-# for it in root.iterfind('channel/item'):
-#     print(it.tag, it.attrib)
-#
-#
-# root2 = doc.find('channel/title')
-# print(root2.tag)
-# print(root2.attrib)
 
