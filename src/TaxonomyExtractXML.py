6d97685db4862807f2b125bd90a01feda2ba5f1e
Jing Li <u6531952@anu.edu.au>
2019-05-02 04:21:01 +1000

    match holotype with names

--- src/TaxonomyExtractXML.py
+++ src/TaxonomyExtractXML.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
 
+
 def get_tag_dict():
     """Reads tags_config.txt and maps each of the tags to their data category label. Returns a dictionary."""
     tag_source_file = open("../tags_config.txt", 'r')
@@ -24,14 +25,13 @@ def get_tag_dict():
 
 
 
-allinfoList=list()
-tempGenSpe=dict()  # {'genus':genus_name,''species':species_name}
-tempGenSpe['genus']=''
-tempGenSpe['species']=''
-df_nonEmpty=pd.DataFrame(columns=['genus','species'])
 
 
-articleName='../26newSpecies.xml'
+
+#articleName='../aTerrestrialFrog.xml'
+#articleName='../26newSpecies.xml'
+#articleName='../NewGeneraOfAustralianStilettoFlies.xml'
+#articleName='../aNewSpeciesOfWesmaeliusKrügerFromMexico.xml'
 tree=ET.parse(articleName)
 root=tree.getroot()
 
@@ -58,10 +58,16 @@ def get_doi():
 
 
 def get_abstract_info():
-    """read the xml's abstract, extract new genus and species, store in a pandas structure"""
 
-    global df_nonEmpty
+    """read the xml's abstract, extract new genus and species, store in a pandas structure"""
     global root
+
+    tempGenSpe=dict()  # {'genus':genus_name,''species':species_name}
+    tempGenSpe['genus']=''
+    tempGenSpe['species']=''
+    df_nonEmpty=pd.DataFrame(columns=['genus','species'])
+
+
     abStructure='./front/article-meta/abstract/p/italic/{http://www.plazi.org/taxpub}taxon-name/{http://www.plazi.org/taxpub}taxon-name-part'
 
 
@@ -76,68 +82,156 @@ def get_abstract_info():
             df_nonEmpty=df_nonEmpty.append(insertRow,ignore_index=True)
             tempGenSpe['genus']=''
             tempGenSpe['species']=''
+    return df_nonEmpty
 
 
 
-def snpSingleInfo(item):
+
+def snp_single_info(item2):
     genus=''
     species=''
+    holotype_and_loc=''
 
-    for subItem in item:
 
-        if subItem.tag=='{http://www.plazi.org/taxpub}taxon-name-part':
+    if item2.tag=='{http://www.plazi.org/taxpub}taxon-treatment':
 
-            if subItem.attrib['taxon-name-part-type']=='genus':
+        for item3 in item2:
 
-                genus=subItem.text
+            if item3.tag=='{http://www.plazi.org/taxpub}nomenclature':
 
-            if subItem.attrib['taxon-name-part-type']=='species':
-                species=subItem.text
+                if item3.findall('{http://www.plazi.org/taxpub}taxon-name') and \
+                        item3.findall('{http://www.plazi.org/taxpub}taxon-status'):
 
-    return (genus,species)
-    # if item.tag=='{http://www.plazi.org/taxpub}taxon-status':
-    #     if item.text in ('sp.n.' or 'n.sp.'):
+                    for item4 in item3:
+                        if item4.tag=='{http://www.plazi.org/taxpub}taxon-name':
 
-def snpInBodySys():
+                            for item5 in item4:
+                                if(item5.tag=='{http://www.plazi.org/taxpub}taxon-name-part'):
+                                    if item5.attrib['taxon-name-part-type']=='genus':
+
+                                        genus=item5.text
+                                        #print(genus)
+
+                                    if item5.attrib['taxon-name-part-type']=='species':
+                                        species=item5.text
+                                        #print(species)
+
+            if item3.tag=='{http://www.plazi.org/taxpub}treatment-sec':
+                if 'holotype' in item3.attrib['sec-type'].lower() or 'material' in item3.attrib['sec-type'].lower():
+
+                    for item41 in item3:
+                        if item41.tag=='p' and holotype_and_loc=='':
+                            if item41.text!=None:
+                                if (('male' in item41.text.lower()) or ('female' in item41.text.lower())) \
+                                        and  ('holotype' in item41.text.lower()):
+                                    holotype_and_loc=item41.text
+                                    break
+                            else:
+                                for item6 in item41:
+                                    if item6.text!=None:
+                                        if (('Holotype' in item6.text) or ('holotype' in item6.tail)):
+                                            if item6.tail!=None:
+
+                                                holotype_and_loc=item6.tail
+                                                break
+                                    if item6.tail!=None:
+                                        if (('male' in item6.tail) or ('female' in item6.tail)):
+                                            holotype_and_loc=item6.tail
+                                            break
+
+
+
+
+    if (genus!='' and species!='' and holotype_and_loc!=''):
+        print('1: '+genus,species)
+        print('2:' +holotype_and_loc)
+        print("-------------------------------------")
+        return [genus,species,holotype_and_loc]
+    else:
+
+        return ([])
+
+
+
+
+def get_info_recursive(item):
+
+
+    for ite1 in item:
+        if ('sec-type' in ite1.attrib):
+
+            if ('Systematic' or 'systematic' or 'Taxonomy' or 'taxonomy' ) in ite1.attrib['sec-type']:
+
+                for ite2 in ite1:
+
+                    row=snp_single_info(ite2)
+
+
+                    if row!=[] and row!=None:
+
+                        return row
+            else:
+
+                get_info_recursive(ite1)
+
+
+
+
+
+def get_info_from_body():
+
     global root
+    df=pd.DataFrame(columns=['genus','species','holotype'])
 
     for item in root.iterfind('./body/sec'):
+        if(item.tag=='sec'):
 
-        if item.attrib['sec-type']=='Systematics':
+            #if ('Systematic' or 'systematic' or 'Taxonomy' or 'taxonomy') in item.attrib['sec-type']:
 
-            for item2 in item:
+            if ('Systematic' in item.attrib['sec-type']) or ('systematic' in item.attrib['sec-type']) or \
+                    ('Taxonomy' in item.attrib['sec-type'])or ('taxonomy' in item.attrib['sec-type']):
 
-                if item2.tag=='{http://www.plazi.org/taxpub}taxon-treatment':
+                for i1 in item:
 
-                    for item3 in item2:
+                    row=snp_single_info(i1)
 
-                        if item3.tag=='{http://www.plazi.org/taxpub}nomenclature':
-                            genus,species='',''
-                            if item3.findall('{http://www.plazi.org/taxpub}taxon-status'):
-
-                                for item4 in item3:
-
-                                    if item4.tag=='{http://www.plazi.org/taxpub}taxon-name':
-
-                                        genus,species=snpSingleInfo(item4)
-                                        # print(genus,species)
-
-                        snpnode=item3.findall('{http://www.plazi.org/taxpub}taxon-status')
-                        for node in snpnode:
-                            if ET.iselement(node):
-                                print(genus,species)
+                    if(row!=None and row!=[]):
 
 
+                        dfseries=pd.Series(row,index=['genus','species','holotype'])
+                        df=df.append(dfseries,ignore_index=True)
 
-snpInBodySys()
+            else:
+                row2=get_info_recursive(item)
+                if(row2!=[]) and row2!=None:
 
 
-# get_abstract_info()
-# print(df_nonEmpty)
+                    dfseries=pd.Series(row2,index=['genus','species','holotype'])
+                    df=df.append(dfseries,ignore_index=True)
+    return df
+
+
+print(get_info_from_body())
+
+
+
+
+def names_in_abstract_and_body():
+    names_in_abstract=get_abstract_info()
+    names_in_body=snp_in_body_sys()
+    df_nonEmpty=pd.merge(names_in_abstract,names_in_body,on=['genus','species'],how='outer')
+    return df_nonEmpty
+
+
+
+
+
+
+#
+# print(names_in_abstract_and_body())
 # get_doi()
 # print('doi is: '+doi)
 # print('ZooBank number is: \n'+zooBankNumber)
-
 
 
 
