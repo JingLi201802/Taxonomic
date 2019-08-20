39dddf4dc496f7a64f95fe5c9c5d58e7440dca14
Tarasom123 <48374339+Tarasom123@users.noreply.github.com>
2019-08-20 22:30:04 +1000

    add comment

--- xml_reader/runTwoFunction.py
+++ xml_reader/runTwoFunction.py
@@ -4,17 +4,19 @@ import os
 
 
 if __name__ == '__main__':
-
+    """get the current path """
     abs_file_path = os.path.abspath(__file__)
-
     parent_dir = os.path.dirname(abs_file_path)
     parent_dir = os.path.dirname(parent_dir)
 
+    """ path that stores xml files"""
     xml_path = parent_dir.replace("\\", "/") + "/Examples/xmls/26newSpecies.xml"
+    # print(xml_path)
 
+    """ call RIE module"""
     lists, ref_list = reference_info_extraction.get_contri_info(xml_path)
-    print(xml_path)
+    reference_info_extraction.write_reference_to_excel(lists, ref_list)
 
-    print(parent_dir)
+
 
 
