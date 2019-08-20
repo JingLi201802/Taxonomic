419bd81b8497ee312fe18fa9d911c38afea82146
Tarasom123 <48374339+Tarasom123@users.noreply.github.com>
2019-08-20 22:23:13 +1000

    change relative path

--- xml_reader/runTwoFunction.py
+++ xml_reader/runTwoFunction.py
@@ -1,13 +1,20 @@
 import reference_info_extraction
-import reader
+# import reader
 import os
 
+
 if __name__ == '__main__':
-    r_list = reference_info_extraction.lists
-    r_ref_list = reference_info_extraction.ref_list
+
     abs_file_path = os.path.abspath(__file__)
+
     parent_dir = os.path.dirname(abs_file_path)
     parent_dir = os.path.dirname(parent_dir)
+
+    xml_path = parent_dir.replace("\\", "/") + "/Examples/xmls/26newSpecies.xml"
+
+    lists, ref_list = reference_info_extraction.get_contri_info(xml_path)
+    print(xml_path)
+
     print(parent_dir)
 
 
