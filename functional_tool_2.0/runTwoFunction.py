89ac606d5fd4321aefdb66f35523cb5bec7c0978
Tarasom123 <48374339+Tarasom123@users.noreply.github.com>
2019-08-20 21:59:54 +1000

    test

--- functional_tool_2.0/runTwoFunction.py
+++ functional_tool_2.0/runTwoFunction.py
@@ -1,4 +1,13 @@
 import reference_info_extraction
 import reader
+import os
+
+if __name__ == '__main__':
+    r_list = reference_info_extraction.lists
+    r_ref_list = reference_info_extraction.ref_list
+    abs_file_path = os.path.abspath(__file__)
+    parent_dir = os.path.dirname(abs_file_path)
+    parent_dir = os.path.dirname(parent_dir)
+    print(parent_dir)
 
 
