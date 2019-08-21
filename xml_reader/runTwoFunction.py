b86b59176eda040b34456107a1ae0154a85cd6d6
Tarasom123 <48374339+Tarasom123@users.noreply.github.com>
2019-08-22 00:36:03 +1000

    zip done + comment

--- xml_reader/runTwoFunction.py
+++ xml_reader/runTwoFunction.py
@@ -21,12 +21,12 @@ if __name__ == '__main__':
     """ call reader module"""
     reader.write_csv("26newSpecies.xml")
 
-    # Create a ZipFile Object
-    with ZipFile(parent_dir.replace("\\", "/")+'/Output/xmlOutput/sample2.zip', 'w') as zipObj2:
+    # Create a zip file
+    with ZipFile(parent_dir.replace("\\", "/")+'/Output/xmlOutput/XMLOutput.zip', 'w') as zipObj:
         # Add multiple files to the zip
-        zipObj2.write(parent_dir.replace("\\", "/")+'/Output/xmlOutput/agents.csv_XmlOutput.csv')
-        # zipObj2.write('test_1.log')
-        # zipObj2.write('test_2.log')
+        zipObj.write(parent_dir.replace("\\", "/")+'/Output/xmlOutput/agents.csv_XmlOutput.csv', "agents.csv")
+        zipObj.write(parent_dir.replace("\\", "/")+'/Output/xmlOutput/references.csv_XmlOutput.csv', "reference.csv")
+        zipObj.write(parent_dir.replace("\\", "/")+'/Output/xmlOutput/26newSpecies.xml_XmlOutput.csv', "readerOutput.csv")
 
 
 
