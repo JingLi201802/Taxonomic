ad1500e325bcface1677b94544b3f8303b63753f
Tarasom123 <48374339+Tarasom123@users.noreply.github.com>
2019-08-22 00:26:34 +1000

    zip try

--- xml_reader/runTwoFunction.py
+++ xml_reader/runTwoFunction.py
@@ -1,3 +1,5 @@
+from zipfile import ZipFile
+
 import reference_info_extraction
 import reader
 import os
@@ -18,6 +20,13 @@ if __name__ == '__main__':
     reference_info_extraction.write_reference_to_excel(lists, ref_list)
     """ call reader module"""
     reader.write_csv("26newSpecies.xml")
+
+    # Create a ZipFile Object
+    with ZipFile(parent_dir.replace("\\", "/")+'/Output/xmlOutput/sample2.zip', 'w') as zipObj2:
+        # Add multiple files to the zip
+        zipObj2.write(parent_dir.replace("\\", "/")+'/Output/xmlOutput/agents.csv_XmlOutput.csv')
+        # zipObj2.write('test_1.log')
+        # zipObj2.write('test_2.log')
 
 
 
