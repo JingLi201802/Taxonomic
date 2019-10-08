b8e54073b31da067777e75a74cca526d439d751e
u5890571 <u5890571@anu.edu.au>
2019-10-08 19:30:07 +1100

    updload src

---
+++ src/functional_tool_2.0/TaxonomyExtractPDF_2.py
@@ -0,0 +1,393 @@
+import PyPDF2
+import os
+import requests
+import pandas as pd
+import re
+import txtCrawl
+
+border_words = ["in", "sp", "type", "and", "are", "figs"]
+pre_buffer = 15
+post_buffer = 20
+created_files = []
+
+# -----------------------------------------------Converting PDF to String----------------------------------------------
+
+
+# Replaced by PDFBox, function is kept so that the program can be executed if java is unavailable
+def create_pdf_reader(path):
+    pdf_file_obj = open(path, 'rb')
+    return PyPDF2.PdfFileReader(pdf_file_obj)
+
+
+# Replaced by PDFBox, function is kept so that the program can be executed if java is unavailable
+def read_all_pages(pdf_reader):
+    page_index = 0
+    result = ""
+    while page_index < pdf_reader.getNumPages():
+        page = (pdf_reader.getPage(page_index).extractText())
+        result = result + page
+        page_index = page_index + 1
+    return result
+
+
+# Replaced by PDFBox, function is kept so that the program can be executed if java is unavailable
+def read_page(page_num, pdf_reader):
+    page = (pdf_reader.getPage(page_num).extractText())
+    page = normalise_spacing(page)
+    return page
+
+
+# Uses PDFBox to convert the PDF to a TXT file for analysis.
+def pdf_to_text(file_path):
+    os.system("java -jar pdfbox-app-2.0.16.jar ExtractText " + file_path)
+    created_files.append(file_path)
+
+
+# Removes temporary files
+def cleanup():
+    for txt in created_files:
+        path = txt.replace(".pdf", ".txt")
+        print ("Removing {}".format(path))
+        try:
+            os.remove(path)
+        except:
+            print("Failed to remove {}".format(path))
+
+# Takes the path to a text file and returns a single-line string
+def normalise_spacing(string):
+    string = string.replace("\n", " ")
+    string = string.replace("  ", " ")
+    return string
+
+
+def remove_punctuation(str):
+    result_str = str.replace(",", "")
+    result_str = result_str.replace(".", "")
+    result_str = result_str.replace(" ", "")
+    result_str = result_str.replace("&", "")
+    result_str = result_str.replace(":", "")
+    result_str = result_str.replace("(", "")
+    result_str = result_str.replace(")", "")
+    return result_str
+
+# ---------------------------------- Extracting Information from Natural Language ----------------------------------
+
+
+def process_string(doc_string):
+    find_new_names(doc_string)
+    find_coordinates(doc_string)
+    reference_index = find_references(doc_string)
+    find_document_data(doc_string, reference_index)
+
+
+# currently does not return anything, just prints out relevant information #
+# If a document mentions a name with sp. n. twice first is usually in abstract while second is description
+# Later be amended to return the location of the string within the document.
+def find_new_names(doc_string):
+    working_index = 0
+    word_list = normalise_spacing(doc_string).split(" ")
+    combined_request_str = ""
+    debugstr=""
+    post_buffer = 20
+    skip_next = False # Used to avoid detection of the same name twice
+    for word in word_list:
+
+        if skip_next:
+            skip_next = False
+            continue
+
+        if word == "nov." or word == "sp.":
+            skip_next = True
+            confidence = 0
+            name = word_list[working_index-pre_buffer: working_index+post_buffer]
+
+            request_str = ""
+            index = 0
+            for name_component in name:
+                if index > pre_buffer and remove_punctuation(name_component.lower()) in border_words and not remove_punctuation(name_component.lower()) == "":
+                    confidence += 1
+                    break
+
+                elif index < pre_buffer and remove_punctuation(name_component.lower()) in border_words and not remove_punctuation(name_component) == "":
+                    request_str = ""
+                    debugstr = ""
+                    confidence = 1
+                    index += 1
+                    continue
+
+                name_component = name_component.replace("&", "%26")
+                if len(remove_punctuation(name_component)) > 0:
+                    request_str = request_str + ("+" + name_component)
+                    debugstr += (name_component + " ")
+                index += 1
+            print("Confidence: {} for name: {}".format(confidence, debugstr))
+
+            combined_request_str = combined_request_str + request_str + "|"
+        working_index = working_index + 1
+
+    r = requests.get('http://parser.globalnames.org/api?q=' + (combined_request_str[1:-1]))
+    return r.json()
+
+
+# Finds high level information about the pdf like author
+# data published, zoobank id etc. Does not look in the references section.
+# Todo: fix issue where two words on different lines are merged during conversion
+def find_document_data(doc_string, reference_index):
+    word_list = doc_string.split(" ")
+    url_list = []
+    for word in word_list[:reference_index]:
+        if word.__contains__("http"):
+            word = word[word.index("http"):]
+            url_list.append(word)
+
+    for url in url_list:
+        if url.__contains__("zootaxa") or url.__contains__("zoobank"):
+            print("Self referencing information: " + url)
+
+
+# Use anystyle.io to analyse references within a txt file.
+# Currently has issues with false positives (Interpreting random sentences as references)
+# TODO: Change command to work on multiple operating systems.
+def find_references(txt_path):
+    command = "anystyle --overwrite -f xml find {} {}".format(txt_path, get_example_path(""))
+    os.system(command)
+
+    # Add to list of files to cleanup later
+    created_files.append(get_example_path(txt_path.split("/")[-1].replace(".txt", ".xml")))
+
+
+# Returns an iterator containing the details and locations of all coordinates of the form xxºxx'xx"[NSEW]
+def find_coordinates(doc_string):
+    file = open(doc_string, 'r', encoding="utf8")
+    string = file.read()
+    # Regex taken from Regexlib.com ("DMS Coordinate by Jordan Pollard")
+    res = re.findall(r"""[0-9]{1,2}[:|°|º][0-9]{1,2}[:|'](?:\b[0-9]+(?:\.[0-9]*)?|\.[0-9]+\b)"?[N|S|E|W]""", string, re.UNICODE)
+    return res
+
+
+# Todo: Given a string index, find the nearby name which that information is most likely to belong to.
+def associate_info_with_name():
+    return None
+
+# -----------------------------------------------File operations--------------------------------------------------
+
+
+def get_root_dir():
+    abs_file_path = os.path.abspath(__file__)
+    parent_dir = os.path.dirname(abs_file_path)
+    parent_dir = os.path.dirname(parent_dir)
+    return parent_dir
+
+
+# Abstract out the identical part of these two functions soonTM
+def get_example_path(pdf_name):
+    # #result = os.path.join(get_root_dir(), "Examples/PDFs/{}".format(pdf_name))
+    # return result.replace("\\", "/")
+    return pdf_name
+
+
+def get_output_path(name):
+    #result = os.path.join(get_root_dir(), "Output/{}/".format(name))
+    result = os.path.join(get_root_dir(), "functional_tool_2.0/csv_folder/{}/".format(name))
+    if not os.path.exists(result):
+        os.makedirs(result)
+    return result.replace("\\", "/")
+
+
+def get_config_path():
+    abs_file_path = os.path.abspath(__file__)
+    parent_dir = os.path.dirname(abs_file_path)
+    parent_dir = os.path.dirname(parent_dir)
+    #return os.path.join(parent_dir, "Configuration")
+    return os.path.join(parent_dir, "functional_tool_2.0/Configuration")
+
+
+def get_configurations():
+    get_key_words(get_config_path())
+
+
+def get_key_words(config_path):
+    key_word_file = "BorderWords.txt"
+    txtCrawl.border_words.clear()
+    file = open(os.path.join(config_path, key_word_file))
+    lines = file.read().split("\n")
+    for line in lines:
+        if line.startswith("#"):
+            continue
+
+        txtCrawl.border_words.append(line)
+
+# ------------------------------------ Handling JSON from GNParser ----------------------------------------------------
+
+
+def parse_json_list(json):
+    name_results = []
+
+    # Change this definition to be dependent on the dictionary mapping below so we don't need to change both each time
+    json_targets = direct_mappings.keys()
+
+    for item in json:
+        print("item:" + str(item) + "\nResult")
+        print(get_json_fields(item, json_targets))
+        name_results.append(get_json_fields(item, json_targets))
+
+    return name_results
+
+# Add support for lists
+def get_json_fields(json, targets):
+    output_dict = dict()
+
+    if not json['parsed']:
+        print("This name was not able to be parsed")
+        output_dict['verbatim'] = "unparsable"
+        return output_dict
+
+
+    for target in targets:
+        index = 0
+        target_path = target.split("/")
+        pointer = json
+
+        for node in target_path:
+            if node.__contains__("[]"):
+                if node.replace("[]", "") in pointer:
+                    pointer = pointer[node.replace("[]", "")][0]
+                    if node == target_path[-1]:
+                        output_dict[target] = pointer
+                else:
+                    break
+
+            else:
+                if node in pointer:
+                    pointer = pointer[node]
+                    if node == target_path[-1]:
+                        output_dict[target] = pointer
+
+                else:
+                    break
+
+            index += 1
+    return output_dict
+
+
+# / means go one level deeper in JSON, [] means that the value should be treated as a list
+direct_mappings = {
+    "verbatim": "verbatim",
+    "details[]/genus/value": "genus",
+    "details[]/specificEpithet/value": "specificEpithet",
+    "details[]/specificEpithet/authorship/value": "scientificNameAuthorship",
+    "canonicalName/value": "taxonomicNameString", # The API and web version give different names for this field
+    "canonicalName/simple": "taxonomicNameString", # Check if it's okay that canonical omits subgenus for subspecies
+    "details[]/infraspecificEpithets[]/value": "infraspecificEpithet",
+    "details[]/infragenericEpithet/value": "infragenericEpithet",
+    "normalized": "taxonomicName",   # ask about this one
+    "details[]/specificEpithet/combinationAuthorship/years[]": "namePublishedInYear",
+
+
+    # Some of these duplciates may not be necessary
+    "details[]/infraspecificEpithets[]/authorship/value": "scientificNameAuthorship",
+    "details[]/specificEpithet/authorship/value": "scientificNameAuthorship",
+    "details[]/specificEpithet/authorship/combinationAuthorship/authors[]": "combinationAuthorship",
+    "details[]/specificEpithet/authorship/basionymAuthorship/authors[]": "basionymAuthorship",
+    "details[]/infraspecificEpithets[]/authorship/combinationAuthorship/authors[]": "combinationAuthorship",
+    "details[]/infraspecificEpithets[]/authorship/basionymAuthorship/authors[]": "basionymAuthorship",
+
+    # Choose the year from the most relevant author, these entries overwrite previous so order is important
+    "details[]/specificEpithet/authorship/basionymAuthorship/year/value": "namePublishedInYear",
+    "details[]/specificEpithet/authorship/combinationAuthorship/year/value": "namePublishedInYear",
+    "details[]/infraspecificEpithets[]/authorship/combinationAuthorship/year/value": "namePublishedInYear",
+    "details[]/infraspecificEpithets[]/authorship/basionymAuthorship/year/value": "namePublishedInYear",
+}
+
+
+def deduce_tnu_values(df, name_results):
+    index = 1
+
+    while index < df.size and not isinstance(df.at[index, 'scientificName'], float):
+
+        # Uninomial -----------
+        # Todo: change functionality so it detects uninomial- only 1 rank
+        df.at[index, "uninomial"] = (df.at[index, 'scientificName'].split(" ")) == 1
+
+        # TaxonRank ----------
+        # Later need to add support for discovering new families and also tidy up and abstract
+        if not isinstance(df.at[index, 'infraspecificEpithet'], float):
+            df.at[index, "taxonRank"] = 'infraspecificEpithet'
+
+        elif not isinstance(df.at[index, 'specificEpithet'], float):
+            df.at[index, "taxonRank"] = 'specificEpithet'
+
+        elif not isinstance(df.at[index, 'infragenericEpithet'], float):
+            df.at[index, "taxonRank"] = 'infragenericEpithet'
+
+        elif not isinstance(df.at[index, 'genus'], float):
+            df.at[index, "taxonRank"] = 'genus'
+
+        # VerbatimTaxonRank -----------
+        # Temporary liberty before I clarify this point
+        df.at[index, "verbatimTaxonRank"] = df.at[index, "taxonRank"]
+
+        # TaxonomicNameStringWithAuthor ----------
+        if not isinstance(df.at[index, "scientificNameAuthorship"], float):
+            df.at[index, "taxonomicNameStringWithAuthor"] = df.at[index, "scientificName"] + " " + df.at[index, "scientificNameAuthorship"]
+        index += 1
+
+    return df
+
+# ------------------------------------------ Create final output layer -------------------------------------------------
+
+
+# Takes a list of GNParser results (a list of dictionaries) and deduces possible TNU fields for output
+def add_dict_data_to_df(name_results):
+    index = 1
+    # Change this definition later to take columns from direct mappings + some others
+    # Change the index definition to be dynamic
+    df = pd.DataFrame(index=range(1, 10), columns=
+     ["kindOfName", "scientificName", "taxonomicNameStringWithAuthor", "cultivarNameGroup",
+      'verbatim', "uninomial", 'genus', "combinationAuthorship", "basionymAuthorship", "namePublishedInYear",
+      "taxonRank", "verbatimTaxonRank", "taxonomicName", "protonym", "accordingTo", "nameAccordingTo",
+      'infragenericEpithet', 'specificEpithet', "infraspecificEpithet", 'scientificNameAuthorship'])
+
+    for result in name_results:
+        print(result)
+        for key in result:
+            if key in direct_mappings:
+                df.iloc[index][direct_mappings.get(key)] = result.get(key)
+
+        index += 1
+
+    df = deduce_tnu_values(df, name_results)
+    print(df)
+
+    return df
+
+
+def get_csv_output(path):
+
+    pdf_to_text(path)
+    string_file = open(path.replace(".pdf", ".txt"), 'r', encoding="utf8")
+    string = string_file.read()
+    json = parse_json_list(find_new_names(string))
+    df = add_dict_data_to_df(json)
+    df.fillna(0.0).to_csv(get_output_path((path.split("/")[-1])[:-4]))
+
+
+# --------------------------------------------- Testing Code -----------------------------------------------------------
+
+# get_configurations()
+# pdf_to_text(get_example_path("JABG31P037_Lang.pdf"))
+# txtCrawl.get_csv_output_test(get_example_path("JABG31P037_Lang.txt"), direct_mappings, get_output_path("JABG31P037"))
+
+
+# pdf_to_text(get_example_path("JABG31P037_Lang.pdf"))
+# process_string(get_example_path("JABG31P037_Lang.txt"))
+# get_csv_output(get_example_path("JABG31P037_Lang.pdf"))
+# print(find_references(convert(get_example_path("JABG31P037_Lang.pdf"))))
+# (process_string(read_all_pages(
+#    create_pdf_reader(get_example_path("853.pdf")))))
+
+# get_csv_output("JABG31P037_Lang.pdf")
+# get_csv_output("TestNames.pdf")
+# print (find_references(read_all_pages(create_pdf_reader((get_example_path("853.pdf"))))))
+cleanup()
+
---
+++ src/functional_tool_2.0/reference_info_extraction.py
@@ -0,0 +1,197 @@
+import xml.etree.ElementTree as ET
+import os
+from lxml import etree
+import pandas as pd
+import collections
+import xlwt
+import xlrd
+
+"""get the infomation about contribution
+   @param target: absolute url for the input file
+"""
+
+
+def get_contri_info(target):
+    # initialize
+    agents_list = []
+    reference_list = []
+    agents_id = 0
+    reference_id = 0
+    """for multiple articles"""
+    # src_path = os.path.dirname(os.path.realpath(__file__))
+    # print(src_path)
+    # find all the xml files
+    # path_dir = os.listdir(src_path)
+    # print(path_dir)
+    # xml_file = []
+    # for i in path_dir:
+    #    if ".xml" in i:
+    #        xml_file.append(i)
+    # print(xml_file)
+    # xml_file_no = len(xml_file)
+
+    # for file_name in xml_file:
+
+    """for single input"""
+
+    reference_list_item = {}
+    agents = {}
+    reference_id += 1
+    reference_list_item["id"] = "ref:" + "%05d" % reference_id
+    print(" ------------------------ a new file--------------------------")
+    # print(file_name)
+    # target = os.path.join(src_path, file_name)
+    tree = etree.parse(target)
+
+    reference_pool = tree.xpath("//notes//sec//p//text()")
+    doi = tree.xpath("//article-meta//article-id [@pub-id-type='doi']//text()")[0]
+    zooBankNo = tree.xpath("//article-meta//article-id [@pub-id-type='other']//text()")
+    for i in zooBankNo:
+        if "zoobank" in i: zooBankNo = i
+    print(doi)
+    print(zooBankNo)
+
+    # print(type(reference_pool))
+
+    print(reference_pool)
+    separator = ""
+    reference = separator.join(reference_pool)
+
+    print(reference)
+
+    name_year_index = reference.find("(")
+    year_end = reference.find(")")
+    title_end = reference.find(".")
+    name = reference[:name_year_index]
+    title = reference[year_end + 2: title_end]
+    print(title)
+    print(name)
+    reference_list_item["author lookup"] = name
+    year = reference[name_year_index + 1:year_end]
+    print(year)
+    reference_list_item["year"] = year
+    reference_list_item["title"] = title
+
+    fname_list = tree.xpath("//article-meta//contrib-group//name//surname//text()")
+    gname_list = tree.xpath("//article-meta//contrib-group//name//given-names//text()")
+    print(fname_list)
+    reference_list_item["quickRef"] = ", ".join(fname_list) + " " + year
+    print(reference_list_item)
+    print(gname_list)
+
+    no_of_contributor = len(fname_list)
+    if no_of_contributor > 1:
+        agents_id += 1
+        agents["type"] = "foaf:Group"
+        agents["name"] = name
+        agents["id"] = "ag:" + "%05d" % agents_id
+        reference_list_item["author"] = "ag:" + "%05d" % agents_id
+        sub_contributor_list = []
+        member_range = list(range(agents_id + 1, no_of_contributor + agents_id + 2))
+        agents["members"] = ""
+        for i in range(len(member_range) - 2):
+            agents["members"] += "ag:" + "%05d" % member_range[i] + "|"
+        agents["members"] += "ag:" + "%05d" % (member_range.pop() - 1)
+        agents_list.append(agents)
+        for i in range(no_of_contributor):
+            agents_id += 1
+            term = {}
+            term["type"] = "foaf:Person"
+            term["familyName"] = fname_list[i]
+            term["givenName"] = gname_list[i]
+            term["name"] = term["familyName"] + ", " + term["givenName"]
+            term["id"] = "ag:" + "%05d" % agents_id
+            sub_contributor_list.append(term)
+        print(sub_contributor_list)
+        agents_list += sub_contributor_list
+    else:
+        agents_id += 1
+        agents["type"] = "foaf:Person"
+        agents["familyName"] = fname_list[0]
+        agents["givenName"] = gname_list[0]
+        agents["name"] = agents["familyName"] + ", " + agents["givenName"]
+        agents["id"] = "ag:" + "%05d" % agents_id
+        reference_list_item["author"] = "ag:" + "%05d" % agents_id
+        agents_list.append(agents)
+    print(agents)
+    print(no_of_contributor)
+    reference_list_item["doi"] = doi
+    reference_list_item["zoobank number"] = zooBankNo
+    reference_list.append(reference_list_item)
+
+    print(agents_list)
+
+    return agents_list, reference_list
+
+
+"""write the captured data into excel
+   @param a_list : agent infomation list
+   @param r_list : reference information list
+"""
+
+
+def write_excel(a_list, r_list):
+    book = xlwt.Workbook()
+    sheet = book.add_sheet("agents", cell_overwrite_ok=True)
+    reference_sheet = book.add_sheet("references", cell_overwrite_ok=True)
+
+    cols = ["id", "type", "name", "familyName", "givenName", "members"]
+    cols_ref = ["id", "type", "quickRef", "author", "author lookup", "year", "title", "doi", "zoobank number"]
+    title = sheet.row(0)
+    title_ref = reference_sheet.row(0)
+    for i in range(len(cols)):
+        value = cols[i]
+        title.write(i, value)
+    for num in range(len(a_list)):
+        row = sheet.row(num + 1)
+        for index, col in enumerate(cols):
+            if col in a_list[num].keys():
+                value = a_list[num][col]
+                row.write(index, value)
+    for i in range(len(cols_ref)):
+        value = cols_ref[i]
+        title_ref.write(i, value)
+    for num in range(len(r_list)):
+        row = reference_sheet.row(num + 1)
+        for index, col in enumerate(cols_ref):
+            if col in r_list[num].keys():
+                value = r_list[num][col]
+                row.write(index, value)
+    book.save("taxonomy.xls")
+
+""" quick reference column extract"""
+
+def quick_reference(r_list):
+    #print("---------------------------------------")
+    print(r_list)
+    quickRef_list = []
+    for e in r_list:
+        quickRef_list.append(e["quickRef"])
+    print(quickRef_list)
+
+
+##lists, ref_list = get_contri_info("/Users/lijing/Documents/comp8715project/Taxonomic/xml_reader/example.xml")
+
+
+def write_reference_to_excel(a_list, r_list):
+    write_excel(a_list, r_list)
+    abs_file_path = os.path.abspath(__file__)
+    parent_dir = os.path.dirname(abs_file_path)
+    parent_dir = os.path.dirname(parent_dir)
+    # agent_path = os.path.join(parent_dir, "Output/xmlOutput/{}_XmlOutput.csv".format("agents.csv"))
+    # references_path = os.path.join(parent_dir, "Output/xmlOutput/{}_XmlOutput.csv".format("references.csv"))
+    print(parent_dir)
+    agent_path = os.path.join(parent_dir, "csv_folder/{}_XmlOutput.csv".format("agents.csv"))
+    references_path = os.path.join(parent_dir, "csv_folder/{}_XmlOutput.csv".format("references.csv"))
+
+    """excel to csv """
+    agents = pd.read_excel('taxonomy.xls', 'agents', index_col=0)
+    agents.to_csv(agent_path, encoding='utf-8')
+    references = pd.read_excel('taxonomy.xls', 'references', index_col=0)
+    references.to_csv(references_path, encoding='utf-8')
+
+
+#lists, ref_list = get_contri_info("C:/Users/51651/Documents/GitHub/Taxonomic/Examples/xmls/A_new_genus_and_two_new_species_of_miniature_clingfishes.xml")
+
+#quick_reference(ref_list)
+##write_reference_to_excel(lists, ref_list)
