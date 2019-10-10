74637dc0817bdd3c289163c52934249ff4dcd02e
Joshua Trevor <u6405233@anu.edu.au>
2019-10-11 07:18:37 +1100

    Created output files for testing, modified type algorithm

--- src/txtCrawl.py
+++ src/txtCrawl.py
@@ -5,40 +5,52 @@ import json
 import time
 
 border_words = ["in", "sp", "type", "and", "are", "figs"]
+
 name_list = []  # Make this non global in future
 pre_buffer = 15
 post_buffer = 1
 word_locations = dict()
+name_string_to_verbatim = dict()
+word_to_char = dict()
 
 
 def get_csv_output_test(txt_filepath, direct_mappings, output_dir):
     publication_txt = open(txt_filepath, "r", encoding="utf-8")
     publication_string = publication_txt.read()
     names = parse_json_list(find_new_names(publication_string), direct_mappings)
+    create_word_to_char(publication_string)
 
     tndf = create_taxonomic_names(names, direct_mappings, output_dir)
     create_name_index_list(tndf, publication_string)
+    print("NAME INDEX LIST--------------")
+    print(word_locations)
+    print("name list")
+    print(name_list)
     create_bibliographic_reference(output_dir)
     create_typification(publication_string, output_dir)
     create_taxonomic_name_usages(output_dir)
 
 
 def create_taxonomic_names(names, direct_mappings, output_dir):
-    index = 1
+    index = 0
     taxonomic_names_df = pd.DataFrame(index=range(1, len(names)+2), columns=
     ["taxonomicNameString", "fullNameWithAuthorship", "rank", "uninomial", "genus", "infragenericEpithet",
      "specificEpithet", "infraspecificEpithet", "cultivarNameGroup", "taxonomicNameAuthorship", "combinationAuthorship",
      "basionymAuthorship", "combinationExAuthorship", "basionymExAuthorship", "nomenclaturalStatus", "basedOn",
-     "kindOfName", "nameRegistrationString"])
+     "kindOfName", "nameRegistrationString", "verbatimTaxonomicNameString"])
     print ("name length is {}".format(len(names)))
     # If there is a 1:1 relationship with GNParser result, put it straight into the dataframe
     for result in names:
-        print(result)
         for key in result:
             if key in direct_mappings:
                 taxonomic_names_df.iloc[index][direct_mappings.get(key)] = result.get(key)
-
         index += 1
+
+        # Remove empty rows
+        if isinstance(taxonomic_names_df.at[index, "taxonomicNameString"], float):
+            taxonomic_names_df.drop(index=index)
+            index -= 1
+
 
     # Deduce the missing fields from what we have
     taxonomic_names_df = deduce_taxonomic_name_values(taxonomic_names_df)
@@ -56,9 +68,18 @@ def create_bibliographic_reference(output_dir):
 
 
 def create_typification(doc_string, output_dir):
-    detect_type_descriptions(doc_string)
-    typification_df = pd.DataFrame(index=range(1, 10), columns=["nameUsage, typeOfType, typeName, typificationString"])
+    type_data = detect_type_descriptions(doc_string)
+    typification_df = pd.DataFrame(index=range(1, len(type_data)+2), columns=["nameUsage", "typeOfType", "typeName", "typificationString"])
+    index = 0
+    for name in type_data:
+        type_string, type_type = type_data[name]
+        typification_df.iloc[index]["typeName"] = name
+        typification_df.iloc[index]["typificationString"] = type_string
+        typification_df.iloc[index]["typeOfType"] = type_type
+        index += 1
+
     typification_df.fillna(0.0).to_csv("{}typification.csv".format(output_dir))
+
 
 
 def create_taxonomic_name_usages(output_dir):
@@ -106,9 +127,8 @@ def deduce_taxonomic_name_values(df):
 
 
 # Find the name which most closely precedes a given index
-def associate_info(info_index, doc_string):
-    doc = normalise_spacing(doc_string)
-    best_diff = 10000000
+def associate_info(info_index):
+    best_diff = float('inf')
     parent_name = "none"
 
     for key in word_locations.keys():
@@ -116,7 +136,7 @@ def associate_info(info_index, doc_string):
         if best_diff > difference > 0:
             best_diff = difference
             parent_name = word_locations[key]
-    return parent_name
+    return parent_name, best_diff
 
 
 def normalise_spacing(string):
@@ -128,7 +148,6 @@ def normalise_spacing(string):
 def remove_punctuation(str):
     result_str = str.replace(",", "")
     result_str = result_str.replace(".", "")
-    result_str = result_str.replace(" ", "")
     result_str = result_str.replace("&", "")
     result_str = result_str.replace(":", "")
     result_str = result_str.replace("(", "")
@@ -197,7 +216,8 @@ def find_new_names(doc_string):
             working_index = working_index + 1
             continue
 
-        if word.lower() == "sp." or word.lower() == "gen.":
+        if word.lower() == "sp." and \
+                (word_list[working_index+1].__contains__("n.") or word_list[working_index+1].__contains__("nov")):
             skip_next = True
             confidence = 0
             name = word_list[working_index-pre_buffer: working_index+post_buffer]
@@ -220,19 +240,32 @@ def find_new_names(doc_string):
                 if len(remove_punctuation(name_component)) > 0:
                     request_str = request_str + (" " + name_component)
                     debugstr += (name_component + " ")
-                index += 1
-            print("Confidence: {} for name: {}".format(confidence, debugstr))
 
-            combined_request.append(request_str)  # combined_request_str = combined_request_str + request_str + "|"
+                index += 1
+
+            print("Confidence: {} for name: {}".format(confidence, debugstr))
+            # If name is impossibly short just skip it
+            # In future, if name is impossibly short we should work backwards to extend it.
+            if len(request_str) < 5:
+                working_index = working_index + 1
+                continue
+
+            # If this evaluates to true there's usually some weird formatting stuff like ...........scientific Name
+            if len(request_str[1:]) - len(remove_punctuation(request_str[1:])) > 8:
+                combined_request.append(remove_punctuation(request_str[1:]))
+            else:
+                combined_request.append(request_str[1:])
         working_index = working_index + 1
 
     r = requests.post("http://parser.globalnames.org/api", json.dumps(combined_request))
     print(r)
+    print(r.json())
     return r.json()
 
 
 def detect_type_descriptions(doc_string):
-    word_list = doc_string.split(" ")
+    type_data = dict()
+    word_list = normalise_spacing(doc_string).split(" ")
     # uses_dms_coordinates = count_coordinates() > 2
     print("------------SEARCHING FOR TYPE DESCRIPTION---------------")
     word_index = 0
@@ -240,27 +273,34 @@ def detect_type_descriptions(doc_string):
         trust = False
         if remove_punctuation(word.lower()) == "holotype" or word.lower().__contains__("holo:"):
             end_point = word_index
+
             # If the word holotype seems to be used midway through a sentence it can usually be ignored
             if ["the", "a", "one", "their"].__contains__(word_list[word_index-1].lower()):
-                print("discarded sentence usage")
                 word_index = word_index + 1
                 continue
 
+            # If the holotype is right next to a gender then it is usually a definition
             if set(map(str.lower, map(remove_punctuation, word_list[word_index-3:word_index+3]))) & set(["male", "female"]):
-                print("holotype adjacent to gender")
+                trust = True
+
+            if word.lower().__contains__("holo:"):
                 trust = True
 
             # If the holotype instance is thought to be a definition, search forward for the next instance of paratype:
             if trust:
                 index = 0  # The index relative to the instance of "holotype"
                 while index < 200:
-                    if remove_punctuation(word_list[word_index + index]).lower().__contains__("paratype"):
+                    if word_list[word_index + index].lower().__contains__("paratype") \
+                            or word_list[word_index + index].lower().__contains__("allotype") \
+                            or word_list[word_index + index].lower().__contains__("para:"):
                         end_point = word_index + index - 1
-                        print("End point found. Type string reads: {}".format(word_list[word_index:end_point]))
+                        name, _ = associate_info(word_to_char[word_index])
+                        type_desc = ""
+                        for word in word_list[word_index:end_point]:
+                            type_desc += word + " "
+                        type_data[name] = type_desc, "holotype"
                         break
                     index = index + 1
-
-            print(word_list[word_index])
 
         word_index = word_index + 1
     # Search for variations of holo and paratype
@@ -270,15 +310,26 @@ def detect_type_descriptions(doc_string):
     # to work out if the heading precedes a description.
     # Can also search for "figure or fig" to try and filter out image captions
     print("----------------Finished searching for types---------------")
-    return ""
+    return type_data
 
 
-# Add support for lists
+# Creates a dictionary which is able to convert an index in the form of the mth word into the nth character.
+def create_word_to_char(doc_string):
+
+    word_list = normalise_spacing(doc_string).split(" ")
+    word_index = 0
+    character_index = 0
+    for word in word_list:
+        word_to_char[word_index] = character_index
+        word_index += 1
+        character_index += len(word) + 1
+
+
+
 def get_json_fields(json, targets):
     output_dict = dict()
 
     if not json['parsed']:
-        print("This name was not able to be parsed")
         output_dict['verbatim'] = "unparsable"
         return output_dict
 
@@ -302,6 +353,7 @@ def get_json_fields(json, targets):
                         result = result + ", " + author
 
                     output_dict[target] = result[2:]
+
                 else:
                     break
 
@@ -318,19 +370,41 @@ def get_json_fields(json, targets):
     return output_dict
 
 
-# possible utils
+# Used so regex search terms aren't interpreted as more complicated queries
+def escape_regex_chars(query):
+    result = query.replace(".", "\.")
+    result = result.replace(",", "\,")
+    result = result.replace("(", "\(")
+    result = result.replace(")", "\)")
+    return result
+
+
+def find_document_data(doc_string, reference_index):
+    word_list = normalise_spacing(doc_string).split(" ")
+    url_list = []
+    for word in word_list[:reference_index]:
+        if word.__contains__("http"):
+            word = word[word.index("http"):]
+            url_list.append(word)
+
+    for url in url_list:
+        if url.__contains__("zootaxa") or url.__contains__("zoobank"):
+            print("Self referencing information: " + url)
+
 
 # Check names for overlap, for example new genuses and their subspecies will cause issues
 # Also should account for cases where the genus is abbreviated eg G. astericus and Goodenia astericus
 def create_name_index_list(taxonomic_names_df, doc_string):
+    doc_string = normalise_spacing(doc_string)
     # Create a list of names in the document
     for index, row in taxonomic_names_df.iterrows():
         if isinstance(row['taxonomicNameString'], float):
             continue
-        name_list.append(str(row['taxonomicNameString']))
+        name = str(row['taxonomicNameString'])
+        name_list.append(name)
+        name_string_to_verbatim[name] = str(row["verbatimTaxonomicNameString"])
 
-    print (name_list)
     for name in name_list:
-        result = re.finditer(name, doc_string)
+        result = re.finditer(escape_regex_chars(name_string_to_verbatim[name]), normalise_spacing(doc_string), re.UNICODE)
         for match in result:
             word_locations[match.span()[1]] = name
